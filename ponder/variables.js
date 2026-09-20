ecec1d2ce1e989edbac0b2c848e6f21b2212f974
nathanhancey <145501256+nathanhancey@users.noreply.github.com>
2026-09-19 19:30:37 -0600

    Initial commit

---
+++ ponder/variables.js
@@ -0,0 +1,30 @@
+
+// Hello, I am a comment!
+// Declare a variable. "Let there be light!"
+let age = 22;
+
+// Print the variable to the console.
+console.log(age);
+
+// A constant variable
+const name = "Nathan Hancey";
+// This constant cannot be changed.
+
+console.log(name);
+
+let favoriteColor = "green";
+
+const username = "billybobjoe";
+
+// Scope = where you can reference a variable by name.
+
+if(age == 22) {
+    console.log(username);
+    let favoriteColor = "blue";
+}
+
+console.log(favoriteColor);
+
+// Grab our H1 element from the DOM and change its color to blue.
+document.getElementsByTagName("h1")[0].style.color = "blue";
+
